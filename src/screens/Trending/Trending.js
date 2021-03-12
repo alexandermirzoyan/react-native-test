8da7083087a7a3df286a7f41bc46b801ac2e7eec
Alexandr <alexandr.mirz12@gmail.com>
2021-03-12 18:03:26 +0400

    search at a Trending.js screen

--- src/screens/Trending/Trending.js
+++ src/screens/Trending/Trending.js
@@ -1,30 +1,68 @@
-import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, ScrollView, TextInput } from 'react-native';
 import TrendingItem from "../../components/TrendingItem/TrendingItem";
 import {data} from "../../utils/data";
 
 import { styles } from "./styles";
 
 const Trending = () => {
-  return (
-    <ScrollView style={styles.mainScrollViewItem}>
-      <View style={styles.mainWrapper}>
-        {
-          data.filter(filteringItem => filteringItem.trending).map((item, index) => (
-            <TrendingItem
-              id={item.id}
-              source={item.image}
-              itemText={item.name}
-              price={item.price}
-              inlineStyles={{
-                marginBottom: 20
-              }}
-            />
-          ))
-        }
-      </View>
+  const [trendingData, setTrendingData] = useState([]);
 
-    </ScrollView>
+  const loadInitialData = () => {
+    const filteredData = data.filter(filteringItem => filteringItem.trending);
+    setTrendingData(filteredData);
+  };
+
+  const handleSearch = (inputValue) => {
+    if (inputValue) {
+      let settingSearchedData = [];
+      data.map((element) => {
+        if (element.name.toLowerCase().includes(inputValue.toLowerCase())) {
+          settingSearchedData.push(element);
+        }
+      });
+      if (settingSearchedData.length) {
+        setTrendingData(settingSearchedData);
+      }
+      else {
+        setTrendingData([]);
+      }
+    }
+    else {
+      loadInitialData();
+    }
+  };
+
+  useEffect(() => {
+    loadInitialData();
+  }, []);
+
+  return (
+    <>
+      <TextInput
+        style={{ height: 60, borderColor: '#000', borderWidth: 1 }}
+        placeholder="   Type Name..."
+        onChangeText={handleSearch}
+      />
+      <ScrollView style={styles.mainScrollViewItem}>
+        <View style={styles.mainWrapper}>
+          {
+            trendingData.map((item, index) => (
+              <TrendingItem
+                id={item.id}
+                source={item.image}
+                itemText={item.name}
+                price={item.price}
+                inlineStyles={{
+                  marginBottom: 20
+                }}
+              />
+            ))
+          }
+        </View>
+
+      </ScrollView>
+    </>
   )
 };
 
--- src/screens/Trending/styles.js
+++ src/screens/Trending/styles.js
@@ -8,5 +8,7 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 30,
+    marginBottom: 60,
   }
 });
